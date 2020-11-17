fea4491c82fe874b2512ce9ac168e42e43ad2ec6
Hao Ran Lai <hrlai.ecology@gmail.com>
2020-11-18 09:38:57 +1300

    refactor

--- bonus/compare_priors.R
+++ bonus/compare_priors.R
@@ -1,26 +1,98 @@
 library(greta)
 
-# commonly used priors on sigma
-sigma <- 
+# see https://greta-stats.org/articles/example_models.html#common-bayesian-priors
+horseshoe <- function (tau = 1, dim = NULL) {
+    lambda <- cauchy(0, 1, truncation = c(0, Inf), dim = dim)
+    sd <- tau ^ 2 * lambda ^ 2
+    return(sd)
+}
+
+regularized_horseshoe <- function (tau = 1,  c = 1, dim = NULL) {
+    stopifnot(c > 0)
+    lambda <- cauchy(0, 1, truncation = c(0, Inf), dim = dim)
+    lambda_tilde <- (c^2 * lambda^2) / (c^2 + tau^2 * lambda^2)
+    sd <- tau ^ 2 * lambda_tilde ^ 2
+    return(sd)
+}
+
+
+# commonly used priors on sd
+sd <- 
     list(
-        exponential(1),
-        normal(0, 1, truncation = c(0, Inf)),
-        # student(3, 0, 10, truncation = c(0, Inf)),
-        lognormal(0, 1),  
-        cauchy(0, 1, truncation = c(0, Inf)),
-        cauchy(0, 2, truncation = c(0, Inf))
+        normal10  = normal(0, 10, truncation = c(0, Inf)),
+        normal1   = normal(0, 1, truncation = c(0, Inf)),
+        # student(3, 0, 10, truncation = c(0, Inf)),  # brms
+        lognormal = lognormal(0, 1),  
+        cauchy1   = cauchy(0, 1, truncation = c(0, Inf)),
+        cauchy2   = cauchy(0, 2, truncation = c(0, Inf)),
+        exp1      = exponential(1),
+        ridge     = inverse_gamma(1, 1),  
+        lasso     = exponential(0.5 * gamma(1, 1)**2),  
+        horseshoe = horseshoe(),
+        finnish_horseshoe = regularized_horseshoe()
     )
 
-sigma_sims <- 
-    lapply(sigma, function(x) {
-        calculate(x, nsim = 10000)
-    })
-
-xlim_max <- 10
-par(mfrow = c(5, 1), mar = c(4.5,4,0,0))
-lapply(sigma_sims, function(x) {
-    plot(density(x$x[,,1], from = 0, to = xlim_max), 
-         xlab = "sigma", ylab = "Density", main = "",
-         xlim = c(0, xlim_max), ylim = c(0, 1),
-         bty = "n", col = "red")
+# simulate coef
+coef <- lapply(sd, function(sd) {
+    normal(0, sd)
 })
+
+# simulate coefs
+coef_sims <- lapply(coef, function(x) {
+    calculate(x, nsim = 10000)
+})
+
+plot_lims <- c(-5, 5)
+plot(density(unlist(coef_sims$normal10), 
+             from = plot_lims[1], to = plot_lims[2]),
+     xlim = plot_lims, ylim = c(0, 0.5),
+     main = "", xlab = "X", col = "grey",
+     las = 1, bty = "n")
+lines(density(unlist(coef_sims$normal1), adjust = 2,
+             from = plot_lims[1], to = plot_lims[2]))
+
+plot(density(unlist(coef_sims$normal10), 
+             from = plot_lims[1], to = plot_lims[2]),
+     xlim = plot_lims, ylim = c(0, 0.5),
+     main = "", xlab = "X", col = "grey",
+     las = 1, bty = "n")
+lines(density(unlist(coef_sims$lognormal), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]))
+
+plot(density(unlist(coef_sims$normal10), 
+             from = plot_lims[1], to = plot_lims[2]),
+     xlim = plot_lims, ylim = c(0, 0.5),
+     main = "", xlab = "X", col = "grey",
+     las = 1, bty = "n")
+lines(density(unlist(coef_sims$cauchy1), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]))
+lines(density(unlist(coef_sims$cauchy2), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]))
+
+plot(density(unlist(coef_sims$normal10), 
+             from = plot_lims[1], to = plot_lims[2]),
+     xlim = plot_lims, ylim = c(0, 0.5),
+     main = "", xlab = "X", col = "grey",
+     las = 1, bty = "n")
+lines(density(unlist(coef_sims$exp1), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]))
+
+plot(density(unlist(coef_sims$normal10), 
+             from = plot_lims[1], to = plot_lims[2]),
+     xlim = plot_lims, ylim = c(0, 0.5),
+     main = "", xlab = "X", col = "grey",
+     las = 1, bty = "n")
+lines(density(unlist(coef_sims$ridge), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]))
+lines(density(unlist(coef_sims$lasso), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]))
+
+plot(density(unlist(coef_sims$normal10), 
+             from = plot_lims[1], to = plot_lims[2]),
+     xlim = plot_lims, ylim = c(0, 0.5),
+     main = "", xlab = "X", col = "grey",
+     las = 1, bty = "n")
+lines(density(unlist(coef_sims$horseshoe), adjust = 2,
+              from = plot_lims[1], to = plot_lims[2]), col = "red")
+lines(density(unlist(coef_sims$finnish_horseshoe), adjust = 4,
+              from = plot_lims[1], to = plot_lims[2]), col = "blue")
